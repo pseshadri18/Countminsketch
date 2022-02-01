60205a1ccda268bb05a5b24132831d497ed05272
Vinit-source <vinitgore1234@gmail.com>
2022-02-01 12:19:24 +0530

    swapped delta and epsilon and changed log10 to log2 for width

--- Documents/cs181/HW3/TwitterStream/countmin_test.py
+++ Documents/cs181/HW3/TwitterStream/countmin_test.py
@@ -1,16 +1,16 @@
 from countmin import CountMin
 
 def main():
-    delta = 0.001 # for width 
-    epsilon = .2 # for depth
+    delta = 0.125 # for depth 
+    epsilon = .2 # for width
 
     # make sure increment and estimate work 
     cm = CountMin(epsilon, delta)
     cm1 = CountMin(epsilon, delta)
-    if (cm.width == 3 and cm.depth == 10):
-        print 'correct dimensions'
+    if (cm.width == 10 and cm.depth == 3):
+        print('correct dimensions')
     else:
-        print 'issue with dimensions'
+        print('issue with dimensions')
 
     # words to use for testing     
     w1 = 'hi'
@@ -29,9 +29,9 @@ def main():
 
 
     if (cm.estimate(w1) == 1 and cm.estimate(w2) == 3 and cm.estimate(w3) == 0 and cm.estimate(w4) == 2):
-        print 'increment/estimate look good'
+        print('increment/estimate look good')
     else:
-        print 'there is an issue in increment/estimate'
+        print('there is an issue in increment/estimate')
     
     cm1.increment(w5)
     cm1.increment(w5)
@@ -43,9 +43,9 @@ def main():
 
     # test merge
     if (cm3.estimate(w1) == 2 and cm3.estimate(w2) == 4 and cm3.estimate(w3) == 1 and cm3.estimate(w4) == 2 and cm3.estimate(w5) == 2):
-        print 'merge looks good'
+        print('merge looks good')
     else:
-        print 'there is an issue in merge'
+        print('there is an issue in merge')
 
 if __name__ == '__main__':
     main()
